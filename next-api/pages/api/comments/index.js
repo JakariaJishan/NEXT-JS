89c8cd7761d9d277d8ead36174a58f7471b31df2
JakariaJishan <jakariajishan987@gmail.com>
2022-05-04 16:45:30 +0600

    post request done

--- next-api/pages/api/comments/index.js
+++ next-api/pages/api/comments/index.js
@@ -1,6 +1,16 @@
 import { commnets } from "../../../data/commnets";
 
 export default function handler(req, res) {
+  if (req.method === "GET") {
     res.status(200).json(commnets);
+  } else if (req.method === "POST") {
+    const {comment} = req.body;
+    const newComment = {
+      name: comment,
+      title: comment,
+    };
+    console.log(comment);
+    commnets.push(newComment);
+    res.status(201).json(newComment);
   }
-  
+}
--- next-api/pages/comments/index.js
+++ next-api/pages/comments/index.js
@@ -1,14 +1,24 @@
 import React, { useState } from "react";
 const Commnets = () => {
   const [user, setData] = useState([]);
+  const [comment, setComment] = useState('');
   const fetchData = async () => {
     const res = await fetch("http://localhost:3000/api/comments");
     const data = await res.json();
     setData(data);
   };
-  console.log(user)
+  const handleUser = async () => {
+      const res = await fetch("http://localhost:3000/api/comments",{
+        method : 'POST',
+        headers : { 'Content-Type' : 'application/json'},
+        body : JSON.stringify({comment})
+      });
+      const data = await res.json();
+  }
   return (
     <div>
+        <input type="text" value={comment} onChange={(e) => setComment(e.target.value)} placeholder="enter user" />
+        <button onClick={handleUser}>add user</button>
       <button onClick={fetchData}>load data</button>
       {
           user.map(item => {
