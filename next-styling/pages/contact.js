e48e4bd9f2a3585fd7590f976bba46f0f8c3df65
JakariaJishan <jakariajishan987@gmail.com>
2022-05-05 13:45:25 +0600

    component level style

---
+++ next-styling/pages/contact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from '../styles/contact.module.css';
+const Contact = () => {
+    return (
+        <div className={styles.highlight}>
+            contact page
+        </div>
+    );
+};
+
+export default Contact;
