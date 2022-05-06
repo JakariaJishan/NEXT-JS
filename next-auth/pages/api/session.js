6dd686ad441b867b118aedc757668a19fb81e648
JakariaJishan <jakariajishan987@gmail.com>
2022-05-06 16:52:16 +0600

    securing api routes page done

---
+++ next-auth/pages/api/session.js
@@ -0,0 +1,8 @@
+import { getSession } from "next-auth/react";
+
+export default async function handler(req, res) {
+    const session = await getSession({req})
+    console.log(session);
+    res.status(200).json(session)
+  }
+  
