d372cef0a35c1df629424e465a92b8829f9d9fe6
JakariaJishan <jakariajishan987@gmail.com>
2022-05-06 23:29:17 +0600

    some problm with redirect

--- next-auth/pages/api/auth/[...nextauth].js
+++ next-auth/pages/api/auth/[...nextauth].js
@@ -12,5 +12,10 @@ export default NextAuth({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET
     })
-  ]
+  ],
+  pages:{
+    signIn: '/signin-in'
+  },
+  secret:'secret',
+  
 });
