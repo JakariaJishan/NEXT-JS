ede46568310e081641647610b96f4b75e3b8264c
JakariaJishan <jakariajishan987@gmail.com>
2022-05-06 11:45:24 +0600

    signin signout done with navbar

--- next-auth/components/navbar.js
+++ next-auth/components/navbar.js
@@ -1,16 +1,28 @@
-import Link from 'next/link';
-import React from 'react';
-
+import { signIn, signOut } from "next-auth/react";
+import Link from "next/link";
+import React from "react";
 const Navbar = () => {
-    return (
-        <div>
-            <Link href={'/'}>Home</Link>
-            <Link href={'/about'}>about</Link>
-            <Link href={'/dashboard'}>about</Link>
-            <Link href={'/#'}>login</Link>
-            <Link href={'/#'}>logout</Link>
-        </div>
-    );
+  const handleSignIn = (e) => {
+    e.preventDefault();
+    signIn('github');
+  };
+  const handleSignOut = (e) => {
+    e.preventDefault();
+    signOut();
+  };
+  return (
+    <div>
+      <Link href={"/"}>Home</Link>
+      <Link href={"/about"}>about</Link>
+      <Link href={"/dashboard"}>about</Link>
+      <Link href={"/api/auth/signin"}>
+        <a onClick={handleSignIn}>sign in</a>
+      </Link>
+      <Link href={"/api/auth/signout"}>
+        <a onClick={handleSignOut}>sign out</a>
+      </Link>
+    </div>
+  );
 };
 
 export default Navbar;
