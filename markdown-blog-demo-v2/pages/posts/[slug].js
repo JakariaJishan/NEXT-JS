b331dc3262060aae2ad752d756f2dc1cb268939e
JakariaJishan <jakariajishan987@gmail.com>
2022-06-04 20:27:18 +0600

    traversy media blog nextjs markdown done

--- markdown-blog-demo-v2/pages/posts/[slug].js
+++ markdown-blog-demo-v2/pages/posts/[slug].js
@@ -1,28 +1,51 @@
+import { marked } from "marked";
+import Link from "next/link";
 import React from "react";
 const fs = require("fs");
 const path = require("path");
-const Slug = () => {
-  return <div>the slug page</div>;
+const matter = require("gray-matter");
+const Slug = ({ data: { title, body }, content, slug }) => {
+  return (
+    <div>
+      <Link href="/">
+        <a>
+          <button>go back</button>
+        </a>
+      </Link>
+      <h1>{title}</h1>
+      <p>{body}</p>
+      <div dangerouslySetInnerHTML={{__html: marked(content)}} />
+    </div>
+  );
 };
 
 export default Slug;
 
 export async function getStaticPaths() {
   const files = fs.readdirSync(path.join("posts"));
-  const file = files.map((filename) =>({ 
-     params: { 
-         slug: filename.replace(".md", "")
-     }
+  const file = files.map((filename) => ({
+    params: {
+      slug: filename.replace(".md", ""),
+    },
   }));
   return {
     paths: file,
-    fallback: false
+    fallback: false,
   };
 }
 
-export async function getStaticProps({params}) {
-    
-    return {
-        props: 'sdf'
-    }
+export async function getStaticProps({ params }) {
+  const files = fs.readFileSync(
+    path.join("posts", params.slug + ".md"),
+    "utf-8"
+  );
+  const { data, content } = matter(files);
+
+  return {
+    props: {
+      data,
+      content,
+      slug: params.slug,
+    },
+  };
 }
