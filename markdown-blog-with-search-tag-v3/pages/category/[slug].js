d634896dae04a509398fbc45f7b1de5bda768293
JakariaJishan <jakariajishan987@gmail.com>
2022-06-06 14:02:40 +0600

    some bug occurs in category slug page

--- markdown-blog-with-search-tag-v3/pages/category/[slug].js
+++ markdown-blog-with-search-tag-v3/pages/category/[slug].js
@@ -1,11 +1,37 @@
-import React from 'react';
-
+import matter from "gray-matter";
+import React from "react";
+const fs = require("fs");
+const path = require("path");
 const Category = () => {
-    return (
-        <div>
-            sdf
-        </div>
-    );
+  return <div>sdf</div>;
 };
 
 export default Category;
+
+export async function getStaticPaths() {
+  const files = fs.readdirSync(path.join("posts"));
+  const paths = files.map((filename) => {
+    const markdownWithMeta = fs.readFileSync(
+      path.join("posts", filename),
+      "utf-8"
+    );
+    const {
+      data: { categories },
+    } = matter(markdownWithMeta);
+
+    const temp = categories.map((category) => {
+      return {
+        params: {
+          slug: category,
+        },
+      };
+    });
+    return temp
+  });
+
+  console.log(paths)
+  return {
+    paths,
+    fallback: false,
+  };
+}
--- markdown-blog-with-search-tag-v3/posts/how-to-add-search-bar-functionality-in-ghost-cms-help-of-searchinghost.md
+++ markdown-blog-with-search-tag-v3/posts/how-to-add-search-bar-functionality-in-ghost-cms-help-of-searchinghost.md
@@ -1,17 +1,11 @@
 ---
-author : "Rajdeep Singh"
-date : "2021-10-11T10:41:36Z"
-description : ""
-draft : true
-slug : "how-to-add-search-bar-functionality-in-ghost-cms-help-of-searchinghost"
-title : "How to Add Search bar functionality in Ghost Cms Help of SearchinGhost"
-
+author: "Rajdeep Singh"
+date: "2021-10-11T10:41:36Z"
+categories: ["ghost"]
+description: ""
+draft: true
+slug: "how-to-add-search-bar-functionality-in-ghost-cms-help-of-searchinghost"
+title: "How to Add Search bar functionality in Ghost Cms Help of SearchinGhost"
 ---
 
-
-
-
 https://github.com/gmfmi/searchinGhost
-
-
-
